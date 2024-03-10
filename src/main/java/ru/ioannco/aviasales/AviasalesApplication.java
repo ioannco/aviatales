048b98f0ec69eba8d725324117ba957c7470439d
ioannco <cheremisenov96@gmail.com>
2024-03-10 16:25:25 +0300

    Init

---
+++ src/main/java/ru/ioannco/aviasales/AviasalesApplication.java
@@ -0,0 +1,13 @@
+package ru.ioannco.aviasales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AviasalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AviasalesApplication.class, args);
+	}
+
+}
