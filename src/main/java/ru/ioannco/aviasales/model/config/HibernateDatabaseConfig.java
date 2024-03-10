668e2e24d1b1e13afc9ca601b6e46e79e70e3931
ioannco <cheremisenov96@gmail.com>
2024-03-10 20:28:05 +0300

    Refactor database configuration
    Group credentials in the separate config bean

--- src/main/java/ru/ioannco/aviasales/model/config/HibernateDatabaseConfig.java
+++ src/main/java/ru/ioannco/aviasales/model/config/HibernateDatabaseConfig.java
@@ -1,6 +1,6 @@
 package ru.ioannco.aviasales.model.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -13,12 +13,8 @@ import java.util.Properties;
 @Configuration
 @EnableTransactionManagement
 public class HibernateDatabaseConfig {
-    @Value("${avia_db_url:localhost}")
-    private String db_url;
-    @Value("${avia_db_username}")
-    private String db_username;
-    @Value("${avia_db_password}")
-    private String db_password;
+    @Autowired
+    DatabaseCredentialsConfig credentialsConfig;
 
     @Bean(name = "entityManagerFactory")
     public LocalSessionFactoryBean sessionFactory() {
@@ -34,9 +30,12 @@ public class HibernateDatabaseConfig {
     public DataSource oraDataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl(db_url);
-        dataSource.setUsername(db_username);
-        dataSource.setPassword(db_password);
+
+        dataSource.setUrl(credentialsConfig.getUrl());
+        dataSource.setUsername(credentialsConfig.getUsername());
+        dataSource.setPassword(credentialsConfig.getPassword());
+
+        System.out.println(credentialsConfig);
 
         return dataSource;
     }
